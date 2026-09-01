b76a6d8e5a1c6dcfae125f5f4882db9b3bfe4ce4
JaRyon <1062017995@qq.com>
2026-09-01 19:55:51 +0800

    简单测试表盘时间显示

--- User/main.c
+++ User/main.c
@@ -1,22 +1,21 @@
 #include "stm32f10x.h"
 #include "Drv_delay.h"
-#include "Drv_tim.h"
 #include "Inf_oled.h"
 #include "Inf_oled_gfx.h"
-#include "Inf_w25q.h"
-#include "Inf_key.h"
-#include "App_menu.h"
+#include "Inf_rtc.h"
+#include "App_watchface.h"
 
 int main(void)
 {
 	Drv_delay_init();
 	Inf_oled_init();
-	Inf_w25q_init();
-	Drv_tim2_init();
-	Inf_key_init();
+	Inf_rtc_init();
 
 	while (1)
 	{
-		App_menu_main_wheel();
+		Inf_oled_clear();
+		App_watchface_show_time(5, 18);
+		Inf_oled_update();
+		Drv_delay_ms(200);
 	}
 }
