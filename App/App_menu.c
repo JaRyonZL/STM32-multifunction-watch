a5e9590b5ee33eb51508f6fb3890d9995c7361bd
JaRyon <1062017995@qq.com>
2026-09-02 13:07:10 +0800

    删除电压表功能函数占位符，引入电压测量功能头文件

--- App/App_menu.c
+++ App/App_menu.c
@@ -4,6 +4,7 @@
 #include "Inf_key.h"
 #include "Com_oled_res.h"
 #include "App_watchface.h"
+#include "App_voltage.h"
 
 /******************************************************************************
  * 文件名称：App_menu.c（应用层）
@@ -409,7 +410,6 @@ void App_menu_run_wheel(App_menu_option1_t* option)
 /******************************************************************************
  * 应用入口占位函数（C3~C13迁移对应模块后删除，改用各App模块真实实现）
  ******************************************************************************/
-void App_voltage_run(void) {}
 void App_mp3_run(void) {}
 void App_qrcode_wechat(void) {}
 void App_qrcode_zfb(void) {}
